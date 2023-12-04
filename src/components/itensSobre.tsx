91215c710fe57b2c8654e78a0d943d3e49b345e2
Cledson96 <cledson1996@gmail.com>
2023-12-03 23:50:42 -0300

    neww

---
+++ src/components/itensSobre.tsx
@@ -0,0 +1,63 @@
+import { BiTargetLock } from "react-icons/bi";
+import { IoFilter } from "react-icons/io5";
+import { PiLockBold } from "react-icons/pi";
+import { HiUsers } from "react-icons/hi";
+import { FaCheck } from "react-icons/fa6";
+import { RiShakeHandsLine } from "react-icons/ri";
+import { FcBusinessman } from "react-icons/fc";
+import { BsBuildings } from "react-icons/bs";
+
+interface item {
+  icon: JSX.Element;
+  tittle: string;
+  text: string;
+}
+export const itens: item[] = [
+  {
+    icon: <IoFilter className="text-orange-500 text-8xl py-2" />,
+    tittle: "BUSCAMOS LICITAÇÕES EM TODO BRASIL",
+    text: "Com mais de 1.2 milhões de oportunidades/ano e um mercado estimado em R$ 300 bilhões, estamos sempre nos atualizando com foco em pesquisa, análise de documentação e nos especializando em leis para atendê-lo com qualidade e excelência. Confira as principais modalidades de licitação que você pode participar: Pregão Eletrônico; Pregão Presencial; Carta Convite;",
+  },
+  {
+    icon: <BiTargetLock className="text-orange-500  text-8xl py-2" />,
+    tittle: "ENCONTRAMOS A LICITAÇÃO PERFEITA PARA SUA EMPRESA",
+    text: "Com mais de 10 anos de experiência, contamos com profissionais especialistas capacitados, que recebem incentivo e aporte de cursos e aprimoramento constantes, para atender todas as fases da licitação.",
+  },
+  {
+    icon: <PiLockBold className="text-orange-500  text-8xl py-2" />,
+    tittle:
+      "PARA NÓS, SEGURANÇA E ESTABILIDADE NOS SERVIÇOS PRESTADOS SÃO FUNDAMENTAIS",
+    text: "Contamos com sistema de internet redundante (100% de conectividade para que não haja perda ou falta de conexão no momento dos processos licitatórios), servidor exclusivo, firewall, appliance (conexão segura sempre).",
+  },
+  {
+    icon: <HiUsers className="text-orange-500  text-8xl py-2" />,
+    tittle:
+      "NOSSA EQUIPE FILTRA AS LICITAÇÕES E ALERTA VOCÊ PARA UMA OPORTUNIDADE QUE COMBINE COM O SEU NEGÓCIO",
+    text: "O nosso maior propósito é facilitar o acesso e vencer as burocracias para vencer as licitações que este mercado pode apresentar. Nos aproximamos da sua empresa para entendermos seu produto e encaminharmos somente editais pertinentes, que sejam realmente interessantes para você. Fazemos filtros automatizados juntamente com processos manuais para aprimorar a seleção.",
+  },
+  {
+    icon: <FaCheck className="text-orange-500  text-8xl py-2" />,
+
+    tittle: "FACILIDADE NO MOMENTO DE ESCOLHA DA LICITAÇÃO",
+    text: "Ao se interessar por um edital específico, você só precisa se preocupar em criar a proposta comercial e enviar para a gente. Cada cliente nosso conta com um consultor exclusivo para orientar nos processos licitatórios, tirando as possíveis dúvidas e apresentando as melhores estratégias para participar do edital.",
+  },
+  {
+    icon: <RiShakeHandsLine className="text-orange-500  text-8xl py-2" />,
+
+    tittle: "PARCERIA ENTRE A SIEG E O SEU NEGÓCIO",
+    text: "Após encaminhar a proposta comercial para nós, assumiremos a parte burocrática de documentação e as providências para que sua empresa esteja apta a participar do edital. Nossa equipe traduzirá o documento enviado por você seguindo as orientações da licitação, que funciona como lei para o certame, e enviará ao Governo para que sua empresa participe do edital.",
+  },
+  {
+    icon: <FcBusinessman className="text-orange-500  text-8xl py-2" />,
+
+    tittle: "AO VENCER A LICITAÇÃO, AINDA REPRESENTAMOS A SUA MARCA",
+    text: "Depois de homologada uma licitação a seu favor, é só esperar o pedido de compra. Mas a nossa parceria não acaba aí, ainda ajudamos você a divulgar estas vendas que podem ser replicadas (adesão de atas) por outros órgãos, de acordo com o interesse das partes, o que pode multiplicar os seus negócios ainda mais.",
+  },
+  {
+    icon: <BsBuildings className="text-orange-500  text-8xl py-2" />,
+
+    tittle:
+      "O NOSSO MAIOR OBJETIVO É CRIAR RELACIONAMENTO SÓLIDO COM SUA EMPRESA",
+    text: "Trabalhamos sempre com contrato claro, objetivo e transparente para que ambas as partes se sintam seguras em continuar a parceria de negócio. Este contrato pode ser encerrado sem multa e sem carência, só é necessário avisar com 30 dias de antecedência. Mais um ponto importante é que você só paga a comissão após receber, isto justamente para que seja uma negociação justa e sem risco.",
+  },
+];
